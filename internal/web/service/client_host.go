203e73eebf4b1c5bf52ee827a5944321b561aeb9
bigbossstard <bigbossstard@gmail.com>
2026-09-23 10:11:28 +0300

    style(client-host): group third-party imports

--- internal/web/service/client_host.go
+++ internal/web/service/client_host.go
@@ -4,10 +4,11 @@ import (
 	"errors"
 	"strings"
 
+	"gorm.io/gorm"
+
 	"github.com/mhsanaei/3x-ui/v3/internal/database"
 	"github.com/mhsanaei/3x-ui/v3/internal/database/model"
 	"github.com/mhsanaei/3x-ui/v3/internal/util/common"
-	"gorm.io/gorm"
 )
 
 // ClientHostService manages the optional client -> HostGroup assignment.
